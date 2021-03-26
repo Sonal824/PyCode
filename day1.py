dc63e6931ccd20a1483de80bf47e54c6595ffd94
Chintan Parikh <chintanparikh@Chintans-MacBook-Air.local>
2021-03-26 20:49:28 +0530

    day1

---
+++ day1.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the sockMerchant function below.
+def sockMerchant(n, ar):
+    hashtable = [];
+    returncount = 0;
+    for i in ar:
+        if (not i in hashtable):
+            hashtable.append(i)
+            totalcount = 0
+            for j in ar:
+                if(i == j):
+                    totalcount = totalcount + 1
+            if(totalcount > 1):
+                returncount += totalcount // 2;
+    return returncount;  
+            
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
